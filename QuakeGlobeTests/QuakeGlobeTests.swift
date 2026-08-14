dc147d643886ecb5d09b18275131a955d4ec06b0
Lucas Tamagnone <316811881+lucascompiles@users.noreply.github.com>
2026-08-14 01:10:02 -0300

    Initial Commit

---
+++ QuakeGlobeTests/QuakeGlobeTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuakeGlobeTests.swift
+//  QuakeGlobeTests
+//
+//  Created by Lucas on 14/08/26.
+//
+
+import Testing
+@testable import QuakeGlobe
+
+struct QuakeGlobeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
